6c5cf57f24f9d30642413703f0a59aaa3de58e8c
flangloi <flangloi@teksystems.com>
2016-04-27 13:36:00 -0400

    jQuery Test Project

---
+++ scripts/app.js
@@ -0,0 +1,20 @@
+/**
+ * Created by flangloi on 2016-04-27.
+ */
+$(function(){
+   $("h2").text("jQuery Testsss!!!");
+
+    $("p:first").text($("p:first").text() + " - Added Value");
+
+    $("#btn1").click(function(){
+        $("ul li:first").hide();
+    });
+
+    $("#btn2").click(function(){
+        $("ul li:first-child").hide();
+    });
+
+    $("#btn3").click(function(){
+        $("[href]").hide();
+    });
+});
